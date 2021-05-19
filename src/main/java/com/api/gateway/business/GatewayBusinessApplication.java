57b7152160c3330a4c7139e4c1d4d761eb52d0a9
Amanda Pires <amandoca_779@hotmail.com>
2021-05-18 22:35:31 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/main/java/com/api/gateway/business/GatewayBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.api.gateway.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gateway/business/GatewayBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
